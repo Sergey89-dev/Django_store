0d68e2e69a39a3748e99b23ad20130a24d051b13
testdriver <ssmerkalov3@gmail.com>
2022-09-05 12:50:10 +0300

    add Stade2

--- app_actually/mainapp/views.py
+++ app_actually/mainapp/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 # Контролер задается функцией
 
 def index(request): #  В качестве аргумента идет request
-	return render(request, 'mainapp/index.html') #  Возвращается рендер из папки\
+	context = {
+		'title': 'Главная'
+	}
+	return render(request, 'mainapp/index.html', context=context) #  Возвращается рендер из папки\
 
 def contact(request):
-	return render(request, 'mainapp/contact.html')
+	context = {
+		'title': 'Контакты'
+	}
+	return render(request, 'mainapp/contact.html', context=context)
 
 links_menu = [
 		{
@@ -32,31 +38,36 @@ links_menu = [
 
 def products(request):
 	context = {
-		'links_menu': links_menu
+		'links_menu': links_menu,
+		'title': 'Продукты'
 	}
 	return render(request, 'mainapp/products.html', context=context)
 
 def products_home(request):
 	context = {
-		'links_menu': links_menu
+		'links_menu': links_menu,
+		'title': 'Мебель для дома'
 	}
 	return render(request, 'mainapp/products.html', context=context)
 
 def products_office(request):
 	context = {
-		'links_menu': links_menu
+		'links_menu': links_menu,
+		'title': 'мебель для офиса'
 	}
 	return render(request, 'mainapp/products.html', context=context)
 
 def products_modern(request):
 	context = {
-		'links_menu': links_menu
+		'links_menu': links_menu,
+		'title': 'Современная мебель'
 	}
 	return render(request, 'mainapp/products.html', context=context)
 
 def products_classic(request):
 	context = {
-		'links_menu': links_menu
+		'links_menu': links_menu,
+		'title': 'Классическая мебель'
 	}
 	return render(request, 'mainapp/products.html', context=context)
 
